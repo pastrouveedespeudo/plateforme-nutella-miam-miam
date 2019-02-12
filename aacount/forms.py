a249f0548a2838f6c431ce00119a1cfff0228c61
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-12 19:31:38 +0100

    Update forms.py

--- aacount/forms.py
+++ aacount/forms.py
@@ -1,38 +1,13 @@
 from django import forms
 
 
-
-class ContactForm(forms.Form):
-    sujet = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    envoyeur = forms.EmailField(label="Votre adresse e-mail")
-    renvoi = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé.", required=False)
-
-
-    nom = forms.CharField(max_length=255, label="")
-    prenom = forms.CharField(max_length=255)
-    password = forms.CharField(max_length=255)
-    
-    email = forms.EmailField(max_length=255)
-    
-    addresse = forms.CharField()
-    ville = forms.CharField(max_length=255)
-    code_postale = forms.CharField(max_length=255)
-    
-    sexe = forms.CharField(max_length=8)
-    date = forms.CharField()
-    statut = forms.CharField(max_length=255)
-
-
-
-
-class compte_utilisateur(forms.Form):
+class compte_utilisateur_form(forms.Form):
 
     nom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Nom',
                                                                         'justify':'center'}))
 
                     
-    prenom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'renom',
+    prenom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'prenom',
                                                                         'justify':'center'}))
     
     password = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Password',
