12550005fa4a7f4bb7a55d4a73923c03ef3699d1
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-12 12:08:38 +0100

    Update forms.py

--- aacount/forms.py
+++ aacount/forms.py
@@ -28,18 +28,46 @@ class ContactForm(forms.Form):
 
 class compte_utilisateur(forms.Form):
 
-    nom = forms.CharField(max_length=255)
-    prenom = forms.CharField(max_length=255)
-    password = forms.CharField(max_length=255)
+    nom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Nom',
+                                                                        'justify':'center'}))
+
+                    
+    prenom = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'renom',
+                                                                        'justify':'center'}))
     
-    email = forms.EmailField(max_length=255)
+    password = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Password',
+                                                                        'justify':'center'}))
     
-    addresse = forms.CharField()
-    ville = forms.CharField(max_length=255)
-    code_postale = forms.CharField(max_length=255)
+    email = forms.EmailField(max_length=255,widget=forms.TextInput(attrs={'placeholder': 'Email',
+                                                                        'justify':'center'}))
     
-    sexe = forms.CharField(max_length=8)
-    date = forms.CharField()
-    statut = forms.CharField(max_length=255)
+    addresse = forms.CharField(max_length=300,widget=forms.TextInput(attrs={'placeholder': 'Adresse',
+                                                                            'size':'62'}))
+    
+    ville = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Ville',
+                                                                        'justify':'center'}))
+    
+    code_postale = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Code postal',
+                                                                        'justify':'center'}))
+    
+    sexe = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'placeholder': 'Sexe',
+                                                                        'justify':'center'}))
+    
+    date = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Date de naissance',
+                                                                        'justify':'center'}))
+    
+    statut = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Statut',
+                                                                        'justify':'center'}))
+
+
+
+
+
+
+
+
+
+
+
 
 
