f04a8f950b187e7376c9c4fadd89ad0c0b2d7630
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-13 17:40:21 +0100

    Update views.py

--- mes_allliments/views.py
+++ mes_allliments/views.py
@@ -1,9 +1,64 @@
 from django.shortcuts import render
+from django.db import models
+import requests
+from bs4 import BeautifulSoup
+import json
 
-
+from .models import categorie
 
 def mes_aliments(request):
+            
+    liste = []
+
+    path = "https://fr.openfoodfacts.org/categories"
+    
+    requete = requests.get(path)
+    page = requete.content
+    soup = BeautifulSoup(page, "html.parser")
+
+    for tag in soup.find_all("td"):
+        liste.append(tag.text)
+
+    c = 0
+    for i in range(3):
+        print(liste[c])
+        objet = categorie(name_categorie = liste[c])
+        objet.save()
+        c+=3
+            
     return render(request, 'pages/mes_aliments.html')
 
 def recherche(request):
     return render(request, 'pages/recherche.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
