22a5a29b9d7c7e789cc27baa857b27a2600b4dc9
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-14 09:54:14 +0100

    Update delete_data.py

--- mes_allliments/database/delete_data.py
+++ mes_allliments/database/delete_data.py
@@ -25,14 +25,53 @@ class delete:
 
         print("aliment supprimer hihihi")
 
+    def delete_tables_aliment(self):
+        
+        db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
+        cursor = db.cursor()
+        
+        cursor.execute('''DROP TABLE mes_aliments_aliment''')
+        db.commit()
+
+        print("database supprimer hihihi")
+
+    def delete_data_categorie(self):
+        
+        db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
+        cursor = db.cursor()
+        
+        cursor.execute('''DROP TABLE mes_aliments_categorie''')
+        db.commit()
+
+        print("database supprimer hihihi")
 
 
+    def delete_tables_store(self):
+        
+        db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
+        cursor = db.cursor()
+        
+        cursor.execute('''DROP TABLE mes_aliments_store''')
+        db.commit()
+
+        print("database supprimer hihihi")
 
 
+    def delete_tables_brand(self):
+        
+        db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
+        cursor = db.cursor()
+        
+        cursor.execute('''DROP TABLE mes_aliments_brand''')
+        db.commit()
 
-
+        print("database supprimer hihihi")
 
 
 yo =delete()
 yo.delete_data_categorie()
 yo.delete_data_aliment()
+yo.delete_tables_brand()
+yo.delete_tables_aliment()
+yo.delete_tables_store()
+yo.delete_tables_categorie()
