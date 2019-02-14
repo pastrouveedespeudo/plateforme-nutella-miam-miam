5b517bcdac0e3ccad8e3854199580ac039658f14
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-14 15:59:16 +0100

    Update algo_open.py

--- mes_allliments/algo_open.py
+++ mes_allliments/algo_open.py
@@ -1,48 +1,55 @@
 import sqlite3
 
 
-class openfoodfact:
-
-    def better_nutri(self):
-
-        db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
-        cursor = db.cursor()
 
 
-        value = input("aliment ?")
-        indice_cat = []
+def better_nutri(para):
+
+    liste = []
+
+    db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
+    cursor = db.cursor()
+
+    indice_cat = []
+    
+
+    cursor.execute('''SELECT id_categorie_id
+    FROM mes_aliments_aliment
+    WHERE name_aliment = (?)''',(para,))
+
+    rows = cursor.fetchall()
+    for i in rows:
+        indice_cat.append(i[0])
+
+    #print(indice_cat)
+
+
+    c = 0
+    for i in indice_cat:
         
-
-        cursor.execute('''SELECT id_categorie_id
+        cursor.execute('''SELECT name_aliment, id_categorie_id, nutriscore, image
         FROM mes_aliments_aliment
-        WHERE name_aliment = (?)''',(value,))
+        WHERE id_categorie_id = (?) and nutriscore != "None"
+        ORDER BY nutriscore ASC''', (indice_cat[c],))
 
-        self.rows = cursor.fetchall()
-        for i in self.rows:
-            #print(i[0])
-            indice_cat.append(i[0])
+        db.commit()
+        
+        rows = cursor.fetchall()
+        for i in rows:
+            #print(i)
+            liste.append(i)
 
-        print(indice_cat)
-
-
-        c = 0
-        for i in indice_cat:
             
-            cursor.execute('''SELECT name_aliment, id_categorie_id, nutriscore
-            FROM mes_aliments_aliment
-            WHERE id_categorie_id = (?)
-            ORDER BY nutriscore ASC''', (indice_cat[c],))
+        c+=1
 
-            db.commit()
-            
-            self.rows = cursor.fetchall()
-            for i in self.rows:
-                print(i)
 
-            c+=1
+    liste = liste[:6]
+    return liste
 
 
 
-       
-yo = openfoodfact()
-yo.better_nutri()
+
+
+
+
+    
