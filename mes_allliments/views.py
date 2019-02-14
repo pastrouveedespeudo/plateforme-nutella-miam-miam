ff8af63bb3273e273dcc3de0aab3efca904bec8a
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-14 15:59:33 +0100

    Update views.py

--- mes_allliments/views.py
+++ mes_allliments/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from django.db import models
-
+from .algo_open import *
+import sqlite3
 
 def mes_aliments(request):     
     return render(request, 'pages/mes_aliments.html')
 
 def recherche(request):
 
-    
+    liste_recherche = []
     
     if request.method == "POST":
-        print("choubaCHOUBAAAAAAAAAAAAA")
-        print(request.POST.get('cool'))
 
-        return render(request, 'pages/recherche.html')
+        recherche = request.POST.get('cool')
+        print(recherche)
+        a = better_nutri(recherche)
+        #print(a[0][3])
+        print(a)
+        
+
+
+
+
+        
+        return render(request, 'pages/recherche.html',
+                      {"a":str(a[0][3]),
+                       "b":str(a[1][3])})
 
 
 
