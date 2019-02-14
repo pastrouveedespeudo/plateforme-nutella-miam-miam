49b3f3596f723fade314c0fdd46f75cc10f779fb
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-14 17:03:14 +0100

    Update views.py

--- mes_allliments/views.py
+++ mes_allliments/views.py
@@ -19,13 +19,20 @@ def recherche(request):
         print(a)
         
 
-
-
-
-        
         return render(request, 'pages/recherche.html',
                       {"a":str(a[0][3]),
-                       "b":str(a[1][3])})
+                       "b":str(a[1][3]),
+                       "c":str(a[2][3]),
+                       "d":str(a[3][3]),
+                       "e":str(a[4][3]),
+                       "f":str(a[5][3]),
+                       "aa":str(a[0][0]),
+                       "bb":str(a[1][0]),
+                       "cc":str(a[2][0]),
+                       "dd":str(a[3][0]),
+                       "ee":str(a[4][0]),
+                       "ff":str(a[5][0])
+                       })
 
 
 
