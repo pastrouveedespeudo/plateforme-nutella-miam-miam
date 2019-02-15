90452dc8aaaa62ffd0c1e361a64aacaf42d35165
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-15 19:45:42 +0100

    Update inscription_connection.py

--- aacount/inscription_connection.py
+++ aacount/inscription_connection.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-def inscription(para):
+def inscription_email(para):
 
     liste = []
-    
+ 
     db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
     cursor = db.cursor()
 
@@ -19,14 +19,28 @@ def inscription(para):
     for i in rows:
         liste.append(i)
 
-    print(liste)
 
     return liste
 
 
 def loggin(self):
-    pass
+
+    identifiant = []
+
+    db = sqlite3.connect(r'C:\Users\jeanbaptiste\plateforme_nutella\plateforme_nutella\db.sqlite3')
+    cursor = db.cursor()
 
 
+    cursor.execute('''SELECT nom, password
+                    FROM account_compte
+                    WHERE nom = (?)''', (para,) )
+    
+    db.commit()
 
-inscription("fe@hotmail.fr")
+    rows = cursor.fetchall()
+
+
+    for i in rows:
+        identifiant.append(i)
+
+
