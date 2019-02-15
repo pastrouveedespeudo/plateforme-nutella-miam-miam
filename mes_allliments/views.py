a32a7ab9ab9a0cfcde8891b96c90a6364e2c4333
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-15 10:10:31 +0100

    Update views.py

--- mes_allliments/views.py
+++ mes_allliments/views.py
@@ -3,8 +3,15 @@ from django.db import models
 from .algo_open import *
 import sqlite3
 
-def mes_aliments(request):     
-    return render(request, 'pages/mes_aliments.html')
+
+
+def aliment_det(request):
+    if request.method == "POST":
+        print("ouiIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+        recherche = request.POST.get('produit')
+        print(recherche)
+        
+    return render(request, 'pages/aliment_det.html')
 
 def recherche(request):
 
@@ -16,7 +23,7 @@ def recherche(request):
         print(recherche)
         a = better_nutri(recherche)
         #print(a[0][3])
-        print(a)
+        #print(a)
         
 
         return render(request, 'pages/recherche.html',
@@ -26,12 +33,20 @@ def recherche(request):
                        "d":str(a[3][3]),
                        "e":str(a[4][3]),
                        "f":str(a[5][3]),
+                       
                        "aa":str(a[0][0]),
                        "bb":str(a[1][0]),
                        "cc":str(a[2][0]),
                        "dd":str(a[3][0]),
                        "ee":str(a[4][0]),
-                       "ff":str(a[5][0])
+                       "ff":str(a[5][0]),
+                       
+                       "aaa":str(a[0][4]),
+                       "bbb":str(a[1][4]),
+                       "ccc":str(a[2][4]),
+                       "ddd":str(a[3][4]),
+                       "eee":str(a[4][4]),
+                       "fff":str(a[5][4]),
                        })
 
 
