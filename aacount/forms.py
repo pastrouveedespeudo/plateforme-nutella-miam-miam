29c6c0a7fe01ca46a283cf39982e50139efcee99
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-16 10:59:58 +0100

    Update forms.py

--- aacount/forms.py
+++ aacount/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
+from django.contrib.auth import (
+    authenticate,
+    get_user_model
+    )
+
+user = get_user_model()
 
 class compte_utilisateur_form(forms.Form):
 
@@ -33,6 +39,94 @@ class compte_utilisateur_form(forms.Form):
     
     statut = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Statut',
                                                                         'justify':'center'}))
+
+
+
+
+class userloginform(forms.Form):
+    
+    username = forms.CharField()
+               
+    prenom = forms.CharField(widget=forms.PasswordInput)
+    
+
+    def clean(self, *args, **kwargs):
+
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+
+            if not user:
+                raise forms.ValidationError('non existing')
+
+            if not user.check_password(password):
+                raise forms.ValidationError('non password biatch')
+
+            if not user.is_active:
+                raise forms.ValidationError('non active')
+
+        return super(userloginform, self).clean(*args, **kwargs)
+
+
+
+
+class userregisterform(forms.ModelForm):
+
+    
+    email = forms.EmailField(label="Email adresse")
+    email2 = forms.EmailField(label="Email adresse")
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class mera:
+        model = user
+        fields =[
+            'username',
+            'email',
+            'email2',
+            'password',
+
+            ]
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email2 = self.cleaned_data.get('email2')
+
+        if email != email2:
+            raise forms.ValidationError("pas les meme")
+
+        email_qs = user.objects.filter(email=email)
+        if email_qs.exists():
+            raise forms.ValidationError("existe deja rton email")
+
+        return email
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
