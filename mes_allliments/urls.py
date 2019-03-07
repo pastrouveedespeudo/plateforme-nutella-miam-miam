2185893a9a79c2624a66df5630d4330393da7596
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-07 22:03:15 +0100

    Update urls.py

--- mes_allliments/urls.py
+++ mes_allliments/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls import include
 
 
 urlpatterns = [
-    path(r'mes_aliments', views.mes_aliments),
+    path(r'aliment_det', views.aliment_det),
     path(r'recherche', views.recherche),
+    path(r'mes_aliments', views.mes_aliments),
     ]
