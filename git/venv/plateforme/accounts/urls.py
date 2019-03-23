a4488c2663dc1d0fca0ba7034cfae0d2e1325554
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-23 22:17:58 +0100

    Update urls.py

--- git/venv/plateforme/accounts/urls.py
+++ git/venv/plateforme/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import essais, essais1, login, register_view
+from .views import essais, essais1, login, register_view, mon_compte
 
 urlpatterns = [
     path('/essais', essais),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('/login', login),
     path('/register_view', register_view),
     path('/logout_view', logout_view),
+    path('/mon_compte', mon_compte),
     ]
