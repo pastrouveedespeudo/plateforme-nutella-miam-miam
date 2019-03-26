c2fddfc83cc64731edb5b0a304585e23988b9053
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-26 16:50:52 +0100

    Update settings.py

--- git/venv/plateforme/platforme/settings.py
+++ git/venv/plateforme/platforme/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'mes_aliments'
+    'mes_aliments',
+    'jeux'
 ]
 
 MIDDLEWARE = [
@@ -123,4 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
