e8a66dc057fc913d1e6fc8787952cd77d4146e8d
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-26 21:57:57 +0100

    Update algo_open.py

--- git/venv/plateforme/mes_aliments/algo_open.py
+++ git/venv/plateforme/mes_aliments/algo_open.py
@@ -1,7 +1,10 @@
+import sqlite3
 import psycopg2
 
-def image_aliment(para):
 
+def image_aliment(para):
+    print(para,"00000000000000000000000000000000")
+    print(type(para))
     conn = psycopg2.connect(database="plateforme",
                             user="postgres",
                             host="127.0.0.1",
@@ -10,15 +13,16 @@ def image_aliment(para):
                     
     cur = conn.cursor()
     
-    cur.execute('''SELECT image, name_aliment
-                    FROM public.mes_aliments_aliment
-                    WHERE name_aliment = "{0}"'''.format(para) )
+    cur.execute("""select image, name_aliment
+                from public.mes_aliments_aliment
+                where name_aliment = '{}';
+                """.format(para))
     conn.commit()
     
     rows = cur.fetchall()
 
     image = [i for i in rows]
-
+    print(image,"000000000000000000000000000000000123123")
     return image
 
 def titre_aliment(para):
@@ -31,9 +35,9 @@ def titre_aliment(para):
                 
     cur = conn.cursor()
 
-    cur.execute('''SELECT name_aliment
+    cur.execute("""SELECT name_aliment
                     FROM public.mes_aliments_aliment
-                    WHERE name_aliment = "{}"'''.format(para))
+                    WHERE name_aliment = '{}'""".format(para))
     conn.commit()
     
     rows = cur.fetchall()
@@ -41,7 +45,7 @@ def titre_aliment(para):
 
     titre = [i for i in rows]
         
-
+    print(titre,"123155555555555555555555555555555555555555")
     return titre
 
 
@@ -56,38 +60,38 @@ def better_nutri(para):
     cur = conn.cursor()
 
 
-    cur.execute('''SELECT id_categorie_id
+    cur.execute("""SELECT id_categorie_id
     FROM public.mes_aliments_aliment
-    WHERE name_aliment = "{}"'''.format(para))
+    WHERE name_aliment = '{}'""".format(para))
 
     conn.commit()
 
     rows = cur.fetchall()
-
+    
     indice_cat = [i[0] for i in rows]
 
 
-    #print(indice_cat)
+    print(indice_cat,"44444444444444444444444444444444444444444444")
 
 
     c = 0
     for i in indice_cat:
         
-        cursor.execute('''SELECT name_aliment, id_categorie_id, nutriscore, image,id
+        cur.execute("""SELECT name_aliment, id_categorie_id, nutriscore, image,id
         FROM public.mes_aliments_aliment
-        WHERE id_categorie_id = {} and nutriscore != "None"
-        ORDER BY nutriscore ASC'''.format(indice_cat[c]))
+        WHERE id_categorie_id = {} and nutriscore != 'No_found'
+        ORDER BY nutriscore ASC""".format(indice_cat[c]))
 
-        db.commit()
+        conn.commit()
         
-        rows = cursor.fetchall()
+        rows = cur.fetchall()
 
         liste = [i for i in rows]
 
             
         c+=1
 
-
+    print(liste,"74894984498498498")
     liste = liste[:6]
     return liste
 
@@ -118,24 +122,3 @@ def detail_aliment(value):
 
     return detail
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
