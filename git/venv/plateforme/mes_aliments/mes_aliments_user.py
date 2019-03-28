fc3ffb0292616d7c5858758f46cc816fe72ca189
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-28 12:15:26 +0100

    Update mes_aliments_user.py

--- git/venv/plateforme/mes_aliments/mes_aliments_user.py
+++ git/venv/plateforme/mes_aliments/mes_aliments_user.py
@@ -1,6 +1,33 @@
 import psycopg2
 
 
+def controle_data_aliment(username):
+    
+    conn = psycopg2.connect(database="plateforme",
+                            user="postgres",
+                            host="127.0.0.1",
+                            password="tiotio")
+
+                
+    cur = conn.cursor()
+
+
+    cur.execute("""SELECT COUNT(name_aliment) FROM aliment_de_{0}
+                    """.format(username))
+
+    conn.commit()
+
+    rows = cur.fetchall()
+
+    count_food = [i for i in rows]
+    print(count_food[0][0],"fooooooooooooooooooooooooooooooooooooooood")
+    if count_food[0][0] >= 6:
+        return "nombre de produit suppérieur a 6", False
+    
+    else:
+        return "stockage du produit possible", True
+
+
 def insert_food(username, food_name):
     
     conn = psycopg2.connect(database="plateforme",
