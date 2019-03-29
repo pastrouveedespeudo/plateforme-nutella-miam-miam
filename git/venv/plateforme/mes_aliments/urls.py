59e31a2b6abda9118f7258cb6b224c83daf23328
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-29 11:16:59 +0100

    Update urls.py

--- git/venv/plateforme/mes_aliments/urls.py
+++ git/venv/plateforme/mes_aliments/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from .views import mes_aliments
 from .views import recherche
-
+from .views import aliment_det
+from .views import recherche2
+from .views import remplacement
 
 
 urlpatterns = [
     path('/mes_aliments', mes_aliments),
     path('/recherche', recherche),
-
-
+    path('/aliment_det', aliment_det),
+    path('/recherche2', recherche2),
+    path('/remplacement', remplacement),
 
     ]
