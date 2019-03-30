cf4f7d53cbcdd677a7af7c4ea25dd478d19448c1
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-30 18:47:45 +0100

    Add files via upload

---
+++ git/venv/plateforme/jeu/verification_reponse.py
@@ -0,0 +1,79 @@
+import psycopg2
+
+def verification(aliment):
+    
+    conn = psycopg2.connect(database="plateforme",
+                            user="postgres",
+                            host="127.0.0.1",
+                            password="tiotio")
+
+                
+    cur = conn.cursor()
+
+    
+    cur.execute("""select nutriscore, id
+                    from public.mes_aliments.aliment
+                    where image = '{}'""".format(aliment))
+
+    
+    conn.commit()
+
+    
+    rows = cur.fetchall()
+
+    information = [i for i in rows]
+    
+    return information
+
+
+
+def voir_aliment(id_aliment):
+
+    
+    conn = psycopg2.connect(database="plateforme",
+                            user="postgres",
+                            host="127.0.0.1",
+                            password="tiotio")
+
+                
+    cur = conn.cursor()
+
+    
+    cur.execute("""select * from public.mes_aliments_aliment
+                where id = {}""".format(id_aliment))
+
+    
+    conn.commit()
+
+    
+    rows = cur.fetchall()
+
+    info_aliment = [i for i in rows]
+
+    return info_aliment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
