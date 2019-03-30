e5930485101d96d6d95c041ac2e38ba0d326e1e2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-30 19:12:24 +0100

    Update views.py

--- git/venv/plateforme/jeu/views.py
+++ git/venv/plateforme/jeu/views.py
@@ -5,8 +5,15 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 import json as simplejson
 from django.http import JsonResponse
+from .score import *
+from .verification_reponse import *
+
 
 def jeux(request):
+
+    current_user = request.user
+    point = score(current_user)
+
    
     if request.method == "POST":
 
@@ -16,6 +23,11 @@ def jeux(request):
   
         continuer = request.POST.get('data')
         continuer = str(continuer)
+
+        current_user = request.user
+        point = score(current_user)
+
+        
         if niveau == "Niveau 1":
             print('Choix de niveau: niveau 1')
             un = choix_aliment_niveau_1()
@@ -64,10 +76,36 @@ def jeux(request):
            
 
         elif continuer:
-            print(continuer)
-            print('ouiiiiiiiiiiiiiiii')
 
-            #ptsscore()
+
+            liste = [[],[]]
+            c=0
+            for i in continuer:
+                if i == ',':
+                    c+=1
+                else:
+                    liste[c].append(i)
+
+            liste = ["".join(i) for i in liste]
+
+            
+            print(liste)
+            
+            print("l'utilisateur a choisis : ", liste[0])
+            print("le produit est : ", liste[1])
+            
+            current_user = request.user
+            
+            nutriscore_id = verification(liste[1])
+            print(nutriscore_id[0][0],"0000000000000000000000000000000000000000")
+
+            if nutriscore_id[0][0] == "a":
+                update_score(request.user, 5)
+                print("oui bonne réponse + 5")
+                message = "oui bonne réponse + 5"
+            else:
+                print("non mauvaise réponse")
+                message = "non mauvaise réponse"
             
             un = choix_aliment_niveau_1()
             numero = ["1","2","3","4"]
@@ -88,13 +126,13 @@ def jeux(request):
                 else:
                     j = l = k = ""
                     
-            data = {"image1":i, "image2":j,"image3":k,"image4":l}
-            
+            data = {"image1":i, "image2":j,"image3":k,"image4":l,"message":message}
+            DATA = []
             return JsonResponse(data)
 
 
             
-    return render(request, "jeux.html", {}) 
+    return render(request, "jeux.html", {'score':point}) 
 
 
 
