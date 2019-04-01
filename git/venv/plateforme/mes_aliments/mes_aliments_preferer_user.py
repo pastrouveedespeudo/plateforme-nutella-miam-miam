958db0bfcc9cffc8b56f149428c49dbd97226944
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-01 16:21:57 +0200

    Update mes_aliments_preferer_user.py

--- git/venv/plateforme/mes_aliments/mes_aliments_preferer_user.py
+++ git/venv/plateforme/mes_aliments/mes_aliments_preferer_user.py
@@ -3,11 +3,10 @@ import psycopg2
 def mes_aliment_user(username):
 
 
-    
-    conn = psycopg2.connect(database="plateforme",
-                            user="postgres",
-                            host="127.0.0.1",
-                            password="tiotio")
+    conn = psycopg2.connect(database="ddgh06joqvm83k",
+                                user="giervvxxoatsci",
+                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
+                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
           
     cur = conn.cursor()
 
@@ -18,38 +17,50 @@ def mes_aliment_user(username):
 
     rows = cur.fetchall()
     food_user = [i for i in rows]
-
     food = [i[1] for i in food_user[1:]]
   
     return food
 
-def display_food(liste_aliment):
-    print(liste_aliment)
 
-    conn = psycopg2.connect(database="plateforme",
-                            user="postgres",
-                            host="127.0.0.1",
-                            password="tiotio")
+
+
+def display_food(liste_aliment):
+ 
+    conn = psycopg2.connect(database="ddgh06joqvm83k",
+                                user="giervvxxoatsci",
+                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
+                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
           
     cur = conn.cursor()
 
     liste_ali = []
-
+    
     for i in liste_aliment:
-        cur.execute("""SELECT * FROM public.mes_aliments_aliment
-                    where name_aliment = '{}'
+
+        cur.execute("""SELECT distinct name_aliment,
+                    code_product_food,description,nutriscore,
+                    image,name_store,name_brand
+                    FROM public.mes_aliments_aliment
+                    where LOWER(name_aliment) = '{}'
                     """.format(i))
 
         conn.commit()
         
 
         rows = cur.fetchall()
+        
         for i in rows:
             liste_ali.append(i)
+          
+    c=0
 
+        
+      
+            
+  
 
+ 
     return liste_ali
-
 
 
 
