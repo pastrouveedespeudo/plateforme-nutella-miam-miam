a49c9514fa549bb1afdce77f46f1badb6d58a2d8
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-02 17:18:58 +0200

    Update views.py

--- git/venv/plateforme/platforme/views.py
+++ git/venv/plateforme/platforme/views.py
@@ -5,3 +5,5 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, "home.html", {})
 
+def mention(request):
+    return render(request, "mention.html", {})
