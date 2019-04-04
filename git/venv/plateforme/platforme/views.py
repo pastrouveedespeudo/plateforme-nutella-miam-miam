ed028424d42d0c1398ed5586ea15a448d43102d0
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-04 12:11:34 +0200

    Update views.py

--- git/venv/plateforme/platforme/views.py
+++ git/venv/plateforme/platforme/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.http import Http500
 
-
-def home(request):
+def home(request):   
     return render(request, "home.html", {})
 
 def mention(request):
     return render(request, "mention.html", {})
+
+def handler404(request):
+    return render(request, "error.html", {}, statut=404)
+
+def handler500(request):
+    return render(request, "error.html", {}, statut=500)
