1399b81aa9a24b8e54c4b989a01a45526dfe8ed2
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-09 11:52:12 +0200

    Update algo_open.py

--- git/venv/plateforme/mes_aliments/algo_open.py
+++ git/venv/plateforme/mes_aliments/algo_open.py
@@ -1,32 +1,28 @@
-"""Here we"""
-
+"""Here we discuss with database"""
 import psycopg2
-
+from .config import DATABASE, USER, HOST, PASSWORD
+from .models import aliment
+from django.db import connection
+from accounts.models import foodAccount
 
 def image_aliment(para):
     """Here we search food picture """
  
- 
     para = para.lower()
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
-
-                    
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
     cur = conn.cursor()
-    
+
     cur.execute("""select image, LOWER(name_aliment)
                 from public.mes_aliments_aliment
                 where LOWER(name_aliment) LIKE '%{}%';
                 """.format(para))
-    
+
     conn.commit()
-    
     rows = cur.fetchall()
-
     image = [i for i in rows]
-
 
     return image
 
@@ -35,67 +31,48 @@ def titre_aliment(para):
     """Here we search title picture """
 
     para = para.lower()
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
 
-                
     cur = conn.cursor()
-
     cur.execute("""SELECT LOWER(name_aliment)
                     FROM public.mes_aliments_aliment
                     WHERE LOWER(name_aliment) LIKE '%{}%'""".format(para))
     conn.commit()
-    
     rows = cur.fetchall()
-
-
     titre = [i for i in rows]
-        
-
     return titre
 
 
-
-
 def better_nutri(para):
-
+    """Here we search best nutriscore from category
+    from food search"""
 
     para = para.lower()
-    
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
-
-                
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
     cur = conn.cursor()
-
-
 
     cur.execute("""SELECT name_aliment from mes_aliments_aliment
                 where LOWER(name_aliment) LIKE '%{}%'""".format(para))
     conn.commit()
-
     rows = cur.fetchall()
-
 
     cur.execute("""SELECT name_aliment, id_categorie_id, nutriscore, image,id
         FROM public.mes_aliments_aliment
         WHERE LOWER(name_aliment) = '{}'""".format(rows[0][0].lower()))
 
     conn.commit()
-
     rows = cur.fetchall()
-
     aliment_recherché = [i for i in rows]
-
 
     liste2 = []
     c = 0
     for i in range(20):
-
         cur.execute("""SELECT name_aliment, id_categorie_id, nutriscore, image
         FROM public.mes_aliments_aliment
         WHERE id_categorie_id = {} and nutriscore != 'No_found' and image != 'No_found'
@@ -103,15 +80,10 @@ def better_nutri(para):
         ORDER BY nutriscore ASC""".format(aliment_recherché[0][1], para))
 
         conn.commit()
-        
         rows = cur.fetchall()
-
         liste = [i for i in rows]
-            
-
-            
+ 
         c+=1
-
 
     liste = set(liste)
     liste = list(liste)
@@ -121,60 +93,35 @@ def better_nutri(para):
     return liste
 
 
-def detail_aliment(value):
-    """ """
+def food_details(value):
+    """Here we calling informations about product. Thank to that we
+    can redirect to Openfactfood"""
 
-    detail = []
+    details = aliment.objects.get(image='{}'.format(value))
 
-
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
-
-                
-    cur = conn.cursor()
-
-    cur.execute("""SELECT *
-                    FROM public.mes_aliments_aliment
-                    WHERE image = '{}'""".format(value))
-
-
-    conn.commit()
-
-    rows = cur.fetchall()
-
-    detail = [i for i in rows]
-  
-    return detail
+    return details
 
 
 
 def replace(para):
-    """ """
+    """We replace food from my_food"""
     
     para = para.lower()
-    
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
 
-                
     cur = conn.cursor()
-
 
     cur.execute("""SELECT id_categorie_id
     FROM public.mes_aliments_aliment
     WHERE LOWER(name_aliment) = '{}'""".format(para))
 
     conn.commit()
-
     rows = cur.fetchall()
-    
     indice_cat = [i[0] for i in rows]
     indice_cat = indice_cat[0]
-
 
     c = 0
     for i in range(20):
@@ -186,11 +133,8 @@ def replace(para):
         ORDER BY nutriscore ASC""".format(indice_cat, para))
 
         conn.commit()
-        
         rows = cur.fetchall()
-
         liste = [i for i in rows]
-
             
         c+=1
 
@@ -198,7 +142,6 @@ def replace(para):
     liste = list(liste)
     liste = liste[:6]
  
-   
     return liste
 
 
@@ -207,73 +150,43 @@ def data_replace(request, username, aliment, new_aliment):
 
 
     
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")                
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD)                
     cur = conn.cursor()
 
-
-    
     cur.execute("""UPDATE aliment_de_{}
                 set name_aliment = '{}' where LOWER(name_aliment) = '{}'
-              
-
-
                 """.format(username,  new_aliment, aliment))
 
     
     conn.commit()
 
-        
-def verification_produit_pas_deux_fois(username, produit):
-    """ """ 
 
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
-                
-    cur = conn.cursor()
+def verification_product_not_two(username, produit):
+    """here we check that the food is not already present"""
 
-
+    c = foodAccount.objects.get(name=username)
+    if c.name_aliment1 == produit or c.name_aliment2  == produit or\
+       c.name_aliment3  == produit or c.name_aliment4  == produit or\
+       c.name_aliment5  == produit or c.name_aliment1  == produit:
+        return False    
     
-    cur.execute("""SELECT * from aliment_de_{}
-                WHERE name_aliment LIKE LOWER('%{}%')
-              
-
-
-                """.format(username, produit))
-
-    
-    conn.commit()
-
-
-    rows = cur.fetchall()
-
-    liste = [i for i in rows]
-    if liste == []:
-        return True
-    else:
-        return False
-
 
 def verification_remplacement(username, produit):
-    """ """ 
+    """We verify food isnt already present""" 
 
-    conn = psycopg2.connect(database="ddgh06joqvm83k",
-                                user="giervvxxoatsci",
-                                host="ec2-75-101-133-29.compute-1.amazonaws.com",
-                                password="2d01f5ec86055f0422b819622bbb1e55a4dbd92d88d73ee9954c128b7aa8790c")
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER,
+                            host=HOST,
+                            password=PASSWORD) 
                 
     cur = conn.cursor()
 
-
-    
     cur.execute("""SELECT name_aliment from aliment_de_{}
                 WHERE name_aliment LIKE LOWER('%{}%')"""
                 .format(username, produit))
-
     
     conn.commit()
 
@@ -288,4 +201,7 @@ def verification_remplacement(username, produit):
         return False
 
 
-    
+
+
+
+
