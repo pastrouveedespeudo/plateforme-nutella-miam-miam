c50f02f7784ff75ea7cc4c036f74f7bdd93d72a3
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-04-11 17:29:24 +0200

    Add files via upload

---
+++ git/venv/plateforme/mes_aliments/construction_database/config.py
@@ -0,0 +1,13 @@
+
+
+##DATABASE = "deed0vc5eh1n6g"
+##USER = "hylgxgwfjacgtj"
+##HOST = "ec2-54-225-95-183.compute-1.amazonaws.com"
+##PASSWORD = "2509ffb8ec855b2a37e2d1b80e0521d942219ad4d55a4abb6c1f5093000f6489"
+
+
+DATABASE = "nutella"
+USER = "postgres"
+HOST = "127.0.0.1"
+PASSWORD = "tiotio"
+
